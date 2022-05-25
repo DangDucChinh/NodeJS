b7a10a036b6f41c5e0ea8bba446b08e75ea1df91
Dang Duc Chinh <chinh22082001@gmail.com>
2022-05-25 14:39:55 +0700

    Lab 1.3 response HTML

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log( req);
+    // console.log( req.url, req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>Node jsPage</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
+    res.write('</html>')
+    res.end() ; 
 });
 
-server.listen(100) ; 
+server.listen(3000) ; 

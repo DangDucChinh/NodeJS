f72053bebc0cb600e6a6ca97307d9e64c0b23018
Dang Duc Chinh <chinh22082001@gmail.com>
2022-05-25 16:48:38 +0700

    Lab 1.4 Chuyen huong Request

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const http = require('http');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
     // console.log( req.url, req.headers);
-    res.setHeader('Content-Type','text/html');
+
+    const urlW = req.url;
+    if (urlW === '/') {
+        res.write('<html>')
+        res.write('<head><title>Input Node JS</title></head>');
+        let html = `<body>
+                        <form action="/message" method="POST">
+                          <input type="text" name=""/>
+                          <button type="submit">Send</button>
+                        </form>
+                    </body>`
+        res.write(html);
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    const name = "Dang Duc Chinh";
+    const age = 21;
+    
     res.write('<html>')
     res.write('<head><title>Node jsPage</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
-    res.write('</html>')
+    let html = `<body>
+                        <h1>Hello from my Node.js server!</h1>
+                        <h2>This is my name : ${name} and age :${age}</h2>
+                        <h3>Url request : ${urlW}</h3>
+                    </body>`
+    res.write(html);
+    res.write('</html>');
     res.end() ; 
+    
 });
 
-server.listen(3000) ; 
+server.listen(3000); 

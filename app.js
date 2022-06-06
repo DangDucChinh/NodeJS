d864581100a3080d7387dcf0835bd74066ec9e46
Dang Duc Chinh <chinh22082001@gmail.com>
2022-06-06 14:38:03 +0700

    Lab02_5 xu li body cua request gui toi

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const routes = require("./routes") ;
 const express = require("express") ;
 const { Console } = require('console');
 
+const parseBody = require('body-parser') ; // thứ 1 : tạo parseBody bằng npm install --save parser-body ??
 
 const app = express() ;
 app.use('/',(req, res, next)=>{
     console.log('Luon xu li o day dau tien !') ; 
     next() ; 
 });
+app.use(parseBody.urlencoded({extended: false})) ;  // đây là 1 middleware , chịu trách nhiệm xử lí data từ req ??
 app.use('/add-product',(req, res ,next)=>{
-    res.send("<h1>The Add product page</h1>")
+    res.send(`<form action="/product" method="POST">
+                    <input type="text" name="title"/>
+                    <button type="submit">Add Product<button>
+                </form>`);
 })
+app.use('/product',(req, res , next)=>{
+    console.log(req.body.title) ; // sau khi được chịu trách nhiệm xử lí data , body đã tạo thành 1 đối tượng  
+    res.redirect('/') ; 
+});
 app.use('/',(req, res ,next)=>{
-    res.send("<h1>Đây là slash </h1>")
+    res.send("<h1>Hello from Node JS</h1>");
 });
 
 app.listen(3000) ; 

770eb52eeff1df0bc50624af4381e53db299f915
Dang Duc Chinh <chinh22082001@gmail.com>
2022-06-07 15:39:29 +0700

    Lab02_6 => method request ( post , get ) thay cho use

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use('/add-product',(req, res ,next)=>{
                     <button type="submit">Add Product<button>
                 </form>`);
 })
-app.use('/product',(req, res , next)=>{
-    console.log(req.body.title) ; // sau khi được chịu trách nhiệm xử lí data , body đã tạo thành 1 đối tượng  
+app.post('/product',(req, res , next)=>{
+    console.log(req.body) ; // sau khi được chịu trách nhiệm xử lí data , body đã tạo thành 1 đối tượng  
     res.redirect('/') ; 
 });
 app.use('/',(req, res ,next)=>{

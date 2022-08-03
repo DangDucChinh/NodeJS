ea21ad05611bed0978cb07468754366a1e2a0def
Dang Duc Chinh <chinh22082001@gmail.com>
2022-08-03 18:39:32 +0700

    Lab08_15 Them chuc nang dang nhap , xac thưc mat khau

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,16 +18,36 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+  // User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email }).then(user_on_data => {
+    if (!user_on_data) {
+      // console.log('Ko có user đó !!');
+      return res.redirect('/login');
+    }
+
+    bcryptjs.compare(password, user_on_data.password) // hàm so sánh hai giá trị băm
+      .then(result_compare => { // chỉ nhận true hoặc false
+        if (result_compare) {
+        // console.log('MK Khớp ');
+
+          req.session.isLoggedIn = true;
+          req.session.user = user_on_data; // tạo phiên user bằng user_data , tức phiên mới này sử dụng cookie của user_on_data
+          // như vậy sẽ lấy ra được người dùng đã đăng kí để đăng nhập 
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect('/'); // tạo phiên mới rồi , nếu có lỗi trong việc tạo phiên thì in ra lỗi và chuyển hướng về index do đã xác 
+            // thực được email và mk 
+          });
+        }
+
+
+        res.redirect('/login');
+      })
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
@@ -41,10 +61,10 @@ exports.postSignup = (req, res, next) => {
         // alert('Đã tồn tại user này rồi , đề nghị đăng kí user khác !!!');  
         return res.redirect('/signup');
       }
-      
+
       return bcryptjs.hash(password, 12)
         .then(hashedPassword => {
-          console.log(hashedPassword);
+          // console.log(hashedPassword);
           const user = new User({
             email: email,
             password: hashedPassword,
@@ -59,7 +79,6 @@ exports.postSignup = (req, res, next) => {
     .catch(err => {
       console.log(err);
     })
-
 
 };
 

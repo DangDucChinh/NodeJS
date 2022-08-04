a8a6c18985e587627428ddfa252be16917b713b3
Dang Duc Chinh <chinh22082001@gmail.com>
2022-08-04 13:41:52 +0700

    Lab08_17 Su dung midddlewaare de bao ve router

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
+const isAuth = require('../middleware/is-auth'); // isAuth là 1 hàm dạng (req, res, next) , giống với các controller bên dưới
+
 const express = require('express');
 
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
 
-// /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/add-product',isAuth ,  adminController.getAddProduct);
 
-// /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.get('/products',isAuth ,adminController.getProducts);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/add-product',isAuth , adminController.postAddProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.get('/edit-product/:productId',isAuth , adminController.getEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/edit-product',isAuth , adminController.postEditProduct);
+
+router.post('/delete-product',isAuth , adminController.postDeleteProduct);
 
 module.exports = router;

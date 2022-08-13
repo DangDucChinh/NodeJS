33a08f1449f94a75da04f91bd67390201b6cb310
Dang Duc Chinh <chinh22082001@gmail.com>
2022-08-13 19:25:56 +0700

    Lab10_6 Luu tru trong cơ so du lieu

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,7 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log('play')
   if (!image) {
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
@@ -38,7 +39,7 @@ exports.postAddProduct = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
+    console.log("\n\n=>" + errors.array());
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
@@ -123,15 +124,22 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
+  console.log('vao chua');
   const prodId = req.body.productId;
+  console.log(prodId);
+
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    console.log(errors);
+
+    console.log('=> Có lỗi');
+
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
@@ -139,7 +147,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId
@@ -149,21 +156,29 @@ exports.postEditProduct = (req, res, next) => {
     });
   }
 
-  Product.findById(prodId)
-    .then(product => {
-      if (product.userId.toString() !== req.user._id.toString()) {
-        return res.redirect('/');
-      }
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save().then(result => {
-        console.log('UPDATED PRODUCT!');
-        res.redirect('/admin/products');
-      });
-    })
+  console.log(prodId);
+  Product.findById(prodId).then(pro => {
+    console.log('tìm được ');
+
+    console.log(pro);
+    if (pro.userId.toString() !== req.user._id.toString()) {
+      return res.redirect('/');
+    }
+
+    pro.title = updatedTitle;
+    pro.price = updatedPrice;
+    pro.description = updatedDesc;
+    if (image) {
+      pro.imageUrl = image.path;
+    }
+
+    return pro.save().then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    }); 
+  })
     .catch(err => {
+      console.log('Ko tìm thấy');
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
